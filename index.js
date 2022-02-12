e20c544d511fbdbc4fda6651e608e0b6abb41765
phlukas <lukas.veckys@dfinsolutions.com>
2022-02-12 19:33:04 +0200

    fixed port typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port | 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log('server is started');

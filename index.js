726670914ffb568df8aecb2a401c65de5bf02b86
lukas.veckys <lukas.veckys@dfinsolutions.com>
2022-03-07 21:05:32 +0200

    added test model and sending data on every request

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const Test = require('./models/test.model');
+
+const url = 'mongodb+srv://ZrEqDkEqlf:WGfpHdhNOl@cluster0.fjiti.mongodb.net/main?retryWrites=true&w=majority';
+
+mongoose.connect(
+  url,
+  {
+    useNewUrlParser: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log('Connection to the database succeeded');
+    } else {
+      console.log('Error in connection to database: ' + err);
+    }
+  }
+);
 
 app.listen(port, () => {
   console.log('server is started');
 });
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  var test = new Test();
+  test.name = 'Vardenis';
+  test.email = 'abc@abc.lt';
+  test.save((err, doc) => {
+    if (err) {
+      console.log('Cannnot save test model');
+    } else {
+      res.send(test);
+    }
+  });
 });

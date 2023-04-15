2ea5aaab1c5ba182948770359bcf1b9c79b2ce09
lukas.veckys <58853955+phlukas@users.noreply.github.com>
2023-04-15 19:50:49 +0300

    fixed-messaging

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,11 +1,17 @@
-import { IUser, User } from "../models/user.model";
+import { IUser, User } from '../models/user.model';
 
 export function GetUser(userId: string): Promise<IUser | null> {
-    return new Promise(async (resolve) => {
-        resolve(await User.findById(userId));
-    });
+  return new Promise(async (resolve) => {
+    resolve(await User.findById(userId));
+  });
 }
 
 export function UpdateUser(userId: string, updateModel: any) {
-    User.findByIdAndUpdate(userId, updateModel);
+  User.findByIdAndUpdate(userId, updateModel, (err: unknown) => {
+    if (err) {
+      console.error('Cannnot update user: ' + err);
+    } else {
+      console.log('success updating user: ' + updateModel);
+    }
+  });
 }
--- socketManager.ts
+++ socketManager.ts
@@ -1,52 +1,45 @@
-import { Client, MesssageModel, SearchingModel } from "./types";
-import { findPairAsync, printRemainingUsers, sendMessage } from "./utilities";
-import { chatRoomEventGroup, messageEvent, userEventGroup } from "./constants";
-import { Server, Socket } from "socket.io";
-import { Server as HttpServer } from "http";
-import _ from "lodash";
+import { Client, MesssageModel, SearchingModel } from './types';
+import { findPairAsync, printRemainingUsers, sendMessage } from './utilities';
+import { chatRoomEventGroup, messageEvent, userEventGroup } from './constants';
+import { Server, Socket } from 'socket.io';
+import { Server as HttpServer } from 'http';
+import _ from 'lodash';
 
 export default (httpServer: HttpServer) => {
-    const io = new Server(httpServer, {
-        cors: {
-            origin: "*",
-        },
+  const io = new Server(httpServer, {
+    cors: {
+      origin: '*',
+    },
+  });
+
+  const searchingClients: Client[] = [];
+
+  io.on('connection', (socket: Socket) => {
+    console.log(`Socket with ID ${socket.id} connected.`);
+
+    socket.on(userEventGroup, (searchingModel: SearchingModel) => {
+      searchingClients.push({
+        socket: socket,
+        userId: searchingModel.userId,
+        radius: searchingModel.kilometers,
+      });
+      findPairAsync(searchingClients);
     });
 
-    const searchingClients: Client[] = [];
-
-    io.on("connection", (socket: Socket) => {
-        console.log(`Socket with ID ${socket.id} connected.`);
-
-        socket.on(userEventGroup, (searchingModel: SearchingModel) => {
-            searchingClients.push({
-                socket: socket,
-                userId: searchingModel.userId,
-                radius: searchingModel.kilometers,
-            });
-            findPairAsync(searchingClients);
-        });
-
-        socket.on(chatRoomEventGroup, (messageModel: MesssageModel) => {
-            if (messageModel.type === messageEvent) {
-                console.log("Gauta zinute:");
-                console.log(messageModel);
-                sendMessage(messageModel, io);
-            }
-        });
-
-        const interval = setInterval(function () {
-            socket.emit("time", Date.now()); // This will be deleted in the near future because it was only created to test front-end
-        }, 5000);
-
-        socket.on("disconnect", () => {
-            clearInterval(interval); // This will be deleted in the near future because it was only created to test front-end
-            _.remove(searchingClients, (client: Client) => {
-                return client.socket === socket;
-            });
-
-            console.log(`Socket with ID ${socket.id} disconnected.`);
-
-            printRemainingUsers(searchingClients);
-        });
+    socket.on(chatRoomEventGroup, (messageModel: MesssageModel) => {
+      if (messageModel.type === messageEvent) {
+        sendMessage(messageModel, io, socket);
+      }
     });
+
+    socket.on('disconnect', () => {
+      _.remove(searchingClients, (client: Client) => {
+        return client.socket === socket;
+      });
+
+      console.log(`Socket with ID ${socket.id} disconnected.`);
+
+      printRemainingUsers(searchingClients);
+    });
+  });
 };

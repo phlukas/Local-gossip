9929831d25d65f1b83156c133434666041bf9995
lukas.veckys <58853955+phlukas@users.noreply.github.com>
2023-04-22 12:00:11 +0300

    refactored logging

--- socketEvents/userEventGroup/incomingEvents/startSearchingEvent.ts
+++ socketEvents/userEventGroup/incomingEvents/startSearchingEvent.ts
@@ -77,33 +77,26 @@ async function findPairAsync(searchingClients: Client[]): Promise<boolean> {
 
 export function printRemainingUsers(searchingClients: Client[]) {
   if (searchingClients.length === 0) {
+    console.log('Searching clients list is empty.');
     return;
   }
-  console.log('Ieskantys partneriu vartotojai: ');
+  console.log('Searching clients: ');
   searchingClients.forEach((client: Client) => {
-    console.log(client.userId);
-    console.log(client.radius + 'km');
+    console.log(`ClientId: ${client.userId}\nSocketId: ${client.socket}\nRadius: ${client.radius} km`);
   });
 }
 
 function distance(lat1: number, lat2: number, lon1: number, lon2: number) {
-  // The math module contains a function
-  // named toRadians which converts from
-  // degrees to radians.
   lon1 = (lon1 * Math.PI) / 180;
   lon2 = (lon2 * Math.PI) / 180;
   lat1 = (lat1 * Math.PI) / 180;
   lat2 = (lat2 * Math.PI) / 180;
 
-  // Haversine formula
   const dlon = lon2 - lon1;
   const dlat = lat2 - lat1;
   const a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
   const c = 2 * Math.asin(Math.sqrt(a));
-
-  // Radius of earth in kilometers.
   const r = 6371;
 
-  // calculate the result
   return c * r;
 }

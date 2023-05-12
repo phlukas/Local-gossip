1221dfe9d82ea51eb9b1cec497977bd36d5b5287
lukas.veckys <58853955+phlukas@users.noreply.github.com>
2023-05-12 11:28:13 +0300

    remove chatroom on exit

--- socketEvents/disconnect.event.ts
+++ socketEvents/disconnect.event.ts
@@ -2,10 +2,23 @@ import { Socket } from 'socket.io';
 import { Client } from '../types';
 import { printRemainingUsers } from './incomingEvents/startSearching.event';
 import _ from 'lodash';
+import { ChatRoom } from '../models/chatRoom.model';
 
 export default (searchingClients: Client[], socket: Socket) => {
   _.remove(searchingClients, (client: Client) => {
     return client.socket === socket;
+  });
+
+  let lastAddedRoom;
+
+  socket.rooms.forEach((roomId) => {
+    lastAddedRoom = roomId;
+  });
+
+  ChatRoom.findByIdAndDelete(lastAddedRoom, (err: any) => {
+    if (err) {
+      console.log('Cannot delete a room. Continuing...');
+    }
   });
 
   console.log(`Socket with ID ${socket.id} disconnected.`);
